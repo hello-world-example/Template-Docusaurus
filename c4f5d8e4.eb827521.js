cd6111286c39b16f2fb6c9c3f9048273373837ad
kaibin.yang <kaibin.yang@ttpai.cn>
2020-08-30 13:31:41 +0800

    Deploy website - based on 7980db31e2e3a41fcfebcae1937b173ea427b47b

---
+++ c4f5d8e4.eb827521.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19,5,20],{74:function(n,t,e){"use strict";e.r(t);e(2);var a=e(0),r=e.n(a),u=(e(87),e(92),e(88),e(86),e(91),e(75),e(99));r.a.Fragment;t.default=function(){return r.a.createElement(u.Redirect,{to:"/Template-Docusaurus/docs/"})}},93:function(n,t,e){"use strict";var a=e(89);e.d(t,"a",(function(){return a.a}))}}]);
